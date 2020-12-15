e579afda8aec15668a396f9a9ef2ca723de1aa58
Dane Miller <booma1191.m@gmail.com>
2020-12-15 03:03:17 -0600

    step 3 of orbit-report

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,11 +1,15 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-list',
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
+
 export class OrbitListComponent implements OnInit {
+  @Input() satellites: Satellite[];
 
   constructor() { }
 
